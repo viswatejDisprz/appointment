e972106e7780f82118251ae9dc988f8419b23fb6
viswatejDisprz <viswatej.p@Disprz.com>
2023-11-07 11:39:49 +0530

    Final Presentation commit

--- AppointmentApi/obj/Debug/net8.0/AppointmentApi.AssemblyInfo.cs
+++ AppointmentApi/obj/Debug/net8.0/AppointmentApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppointmentApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dca2824dd72e3d89ee7d7b64e9b3ee8c51123be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b15b13694d82a7ebf73c98ffa39858a343f026ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppointmentApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppointmentApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
